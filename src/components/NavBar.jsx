2041ebc0e3b135740bc9f9c4dd1413c4806f34d3
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-21 17:23:47 -0500

    Add media links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,12 @@ import logo from '../assets/ms-sin-fondo.png';
 
 const NavBar = () => {
   const location = useLocation();
-  console.log(location.pathname)
 
   return (
     <>
       <div className="w-full">
         <nav>
-          <ul className="flex justify-end h-[100px] items-center gap-9 pr-9">
+          <ul className="flex justify-end h-[100px] items-center gap-9 pr-9 text-white primary-font">
             <li className={ location.pathname === "/" ? "text-yellow-600" : "" }>About</li>
             <li className={ location.pathname === "/work" ? "text-yellow-600" : "" }>Work</li>
             <li className={ location.pathname === "/contact" ? "text-yellow-600" : "" }>Contact</li>
@@ -20,10 +19,16 @@ const NavBar = () => {
       </div>
       <div className="absolute top-0 h-full flex flex-col justify-between items-center pb-[10px]">
         <img src={logo} className="w-[100px]" />
-        <div className="flex flex-col gap-9 text-3xl">
-          <BiLogoGmail />
-          <FaGithub />
-          <FaLinkedin />
+        <div className="flex flex-col gap-9 text-3xl text-white">
+          <a href="mailto:manuelsanchez059@gmail.com" target="_blank" rel="noreferrer">
+            <BiLogoGmail />
+          </a>
+          <a href="https://github.com/Luffytaro22" target="_blank" rel="noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/manuel-alejandro-sanchez-sierra/" target="_blank" rel="noreferrer">
+            <FaLinkedin />
+          </a>
         </div>
       </div>
     </>
