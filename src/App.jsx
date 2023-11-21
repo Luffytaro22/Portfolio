39c7109bc9c5a248e0fa617e927fd8e6462dcbb4
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-21 15:36:18 -0500

    Import the navbar in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import NotFound from './components/NotFound'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route index path="/" element={<About />} />
         <Route path="*" element={<NotFound />} />
