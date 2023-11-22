56fa16fcf92611c747429d727787b24aff242527
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-22 17:25:57 -0500

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     extend: {
       backgroundImage: {
         menuBackground: 'linear-gradient(45deg, rgba(24,24,72,1), rgba(48,72,120,1), rgba(0, 0, 0, 0.5))',
-      }
+      },
+      keyframes: {
+        'show-menu': {
+          '0%': { transform: 'translatex(-100%)' },
+          '50%': { transform: 'translatex(-50%)' },
+          '100%': { transform: 'translatex(0)' },
+        },
+        'hide-menu': {
+          '0%': { transform: 'translatex(0)' },
+          '50%': { transform: 'translatex(-50%)' },
+          '100%': { transform: 'translatex(-100%)' },
+        },
+      },
+      animation: {
+        'show-menu': 'show-menu 0.5s linear forwards',
+        'hide-menu': 'hide-menu 0.5s linear forwards',
+      },
     },
   },
   plugins: [],
