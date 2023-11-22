2abe235e653bde0091e9271d19bd803566488d5c
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-22 16:50:00 -0500

    Add a background color for the menu side bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        menuBackground: 'linear-gradient(45deg, rgba(24,24,72,1), rgba(48,72,120,1), rgba(0, 0, 0, 0.5))',
+      }
+    },
   },
   plugins: [],
   important: true,
