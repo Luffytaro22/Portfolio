e6798fcec938729f40f106235df9cfe3b5946d33
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-24 18:54:27 -0500

    Created the logic to add a transition to the elements when the user scrolls

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,55 @@
+import { useLocation } from 'react-router-dom';
 import { IoLogoJavascript } from "react-icons/io5";
-import { FaHtml5, FaCss3Alt, FaReact, FaSass, FaLinkedin } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaSass} from "react-icons/fa";
 import { SiRedux, SiTailwindcss, SiRubyonrails, SiWebpack, SiVisualstudiocode } from "react-icons/si";
 import { DiRuby } from "react-icons/di";
-import { BiLogoPostgresql, BiLogoGmail } from "react-icons/bi";
+import { BiLogoPostgresql } from "react-icons/bi";
 import { FaGitAlt, FaGithub } from "react-icons/fa6";
+import ContactLinks from './subcomponents/ContactLinks';
 
 const About = () => {
+  const location = useLocation();
+  
+  // Function to check if the element is in the window view.
+  const isElementInViewport = (element) => {
+      const rect = element.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+      );
+  }
+
+  // Function to handle the transitions based on the scroll.
+  const handleScroll = () => {
+    const aboutContainer = document.getElementById('about-container');
+    const frontEndContainer = document.getElementById('front-end-container');
+    const backEndContainer = document.getElementById('back-end-container');
+    const techContainer = document.getElementById('technologies-container');
+
+      if (isElementInViewport(aboutContainer)) {
+          aboutContainer.classList.add('animate');
+      }
+      if (isElementInViewport(frontEndContainer)) {
+        frontEndContainer.classList.add('animate');
+      }
+      if (isElementInViewport(backEndContainer)) {
+        backEndContainer.classList.add('animate');
+      }
+      if (isElementInViewport(techContainer)) {
+        techContainer.classList.add('animate');
+      }
+  }
+
+  // Listeners when the user scrolls or when the page loads.
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('load', handleScroll);
+
+  if (location.pathname === "/") {
+    setTimeout(() => {
+      handleScroll();
+    }, 200);
+  }
+
   return (
     <div className="mt-14 text-center text-white px-2 lg:ml-[13rem] flex flex-col gap-12 xl:flex-row xl:gap-4 justify-center items-center">
       <div className="max-w-md" id="about-container">
@@ -14,22 +58,12 @@ const About = () => {
         <h3 className="secondary-font italic">UX/UI Front-end Web developer</h3><br />
         <p className="secondary-font">I&apos;m a junior developer with experience working both in front-end and back-end.</p><br />
         <p className="secondary-font">I enjoy making good-looking and functional applications paying special attention to the user experience, and writing clean and readable code.</p><br />
-        <div className="flex gap-9 text-3xl text-white justify-center">
-          <a href="mailto:manuelsanchez059@gmail.com" target="_blank" rel="noreferrer">
-            <BiLogoGmail />
-          </a>
-          <a href="https://github.com/Luffytaro22" target="_blank" rel="noreferrer">
-            <FaGithub />
-          </a>
-          <a href="https://www.linkedin.com/in/manuel-alejandro-sanchez-sierra/" target="_blank" rel="noreferrer">
-            <FaLinkedin />
-          </a>
-        </div>
+        <ContactLinks/>
       </div>
       <div className="flex flex-col items-center">
         <h3 className="primary-font text-2xl underline text-green-500">My stack</h3>
         <div className="grid justify-center items-center py-8 gap-8 secondary-font lg:grid-cols-2" id="stack-container" >
-          <div className="lg:row-span-2 lg:self-baseline lg:justify-self-center">
+          <div className="lg:row-span-2 lg:self-baseline lg:justify-self-center" id="front-end-container">
             <h4>Front-End</h4>
             <div className="grid-layout">
               <span>
@@ -62,7 +96,7 @@ const About = () => {
               </span>
             </div>
           </div>
-          <div>
+          <div id="back-end-container">
             <h4>Back-End</h4>
             <div className="grid-layout">
               <span>
@@ -79,7 +113,7 @@ const About = () => {
               </span>
             </div>
           </div>
-          <div>
+          <div id="technologies-container">
             <h4>Technologies</h4>
             <div className="grid-layout">
               <span>
