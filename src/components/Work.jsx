9da186dfc7e5918f15725d0289449a637608ac26
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-27 15:32:17 -0500

    Implement the view in desktops

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,33 +6,33 @@ const Work = () => {
   const works = useSelector((state) => state.works.works);
 
   return (
-    <div className="mt-14 text-center text-white px-2">
-      <h2 className="primary-font text-2xl underline text-green-500">My Projects</h2>
-      <div className="flex flex-col items-center gap-12 py-8" id="works-container">
-        {works.map((work, index) => 
-          <div key={index} className="flex flex-col gap-4 w-full max-w-md p-4 secondary-font">
-            <img src={work.image} className="rounded-lg" />
-            <h3 className="primary-font text-xl">{work.name}</h3>
-            <p>{work.description}</p>
-            <ul className="flex flex-wrap justify-center gap-2">
-              {work.tech.map((stack, index) => 
-                <li key={index} className="bg-black py-1 px-2 rounded-md">{stack}</li>
-              )}
-            </ul>
-            <div className="flex justify-around">
-              <a href={work.link} target="_blank" rel="noreferrer" className="flex justify-center items-center gap-2">
-                View Live
-                <IoOpenOutline className="text-3xl" />
-              </a>
-              <a href={work.github} target="_blank" rel="noreferrer" className="flex justify-center items-center gap-2">
-                View repo
-                <FaGithub className="text-3xl" />
-              </a>
+    <div className="mt-14 text-center text-white px-2 lg:ml-[13rem]">
+        <h2 className="primary-font text-2xl underline text-green-500">My Projects</h2>
+        <div className="grid justify-items-center items-start gap-12 py-8" id="works-container">
+          {works.map((work, index) =>
+            <div key={index} className="flex flex-col gap-4 w-full max-w-md p-4 secondary-font h-full justify-between">
+              <img src={work.image} className="rounded-lg" />
+              <h3 className="primary-font text-xl">{work.name}</h3>
+              <p className="text-justify">{work.description}</p>
+              <ul className="flex flex-wrap justify-center gap-2">
+                {work.tech.map((stack, index) =>
+                  <li key={index} className="tech-stacks">{stack}</li>
+                )}
+              </ul>
+              <div className="flex justify-around">
+                <a href={work.link} target="_blank" rel="noreferrer" className="works-links">
+                  View Live
+                  <IoOpenOutline className="text-3xl" />
+                </a>
+                <a href={work.github} target="_blank" rel="noreferrer" className="works-links">
+                  View repo
+                  <FaGithub className="text-3xl" />
+                </a>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
   );
 };
 
