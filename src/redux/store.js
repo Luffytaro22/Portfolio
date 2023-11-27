3a40cac623db53d29d8bbf0d8da47511146ef745
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-11-27 10:21:05 -0500

    Add a reducer property in the store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import worksReducer from './works/WorksSlice';
 
 const store = configureStore({
-  works: worksReducer,
+  reducer: {
+    works: worksReducer,
+  }
 });
 
 export default store;
